1eda095be9ffcfacec74b582a29e67b0a4cdd9ee
kumub <ma.okamoto.yf7@gmail.com>
2025-10-09 01:24:25 +0900

    出品・商品の状態

--- src/app/Http/Controllers/SellController.php
+++ src/app/Http/Controllers/SellController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Item;
 
 class SellController extends Controller
 {
     public function sell(Request $request)
     {
         $categories = Category::all();
-        return view('sell', compact('categories'));
+        $labels = Item::CONDITION;
+        $items = Item::all();
+        return view('sell', compact('categories', 'items', 'labels'));
     }
 }
--- src/resources/views/sell.blade.php
+++ src/resources/views/sell.blade.php
@@ -1,38 +1,55 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/css.sellcss') }}">
+<link rel="stylesheet" href="{{ asset('css/sell.css') }}">
 @endsection
 
 @section('content')
 <div class="sell">
-    <!-- タイトル -->
-    <div class="sell__title">
-        <h2>商品の出品</h2>
-    </div>
-    <!-- 画像 -->
-    <form class="sell-form" action="">
+    <h2 class="sell__title">商品の出品</h2>
+
+    <form class="sell-form" action="/sell/store" method="post" enctype="multipart/form-data">
         @csrf
-        <div class="sell__image-group">
-            <label for="image_path">商品画像</label>
+
+        <!-- 画像 -->
+        <div class="sell__group">
+            <label class="sell__group-label" for="image_path">商品画像</label>
             <div class="sell__image">
-                <label class="sell__image-choice" for="image_path">画像を選択する</label>
+                <label class="sell__image-button" for="image_path">画像を選択する</label>
                 <input type="file" id="image_path"  name="image_path" accept="image/*" hidden/>
             </div>
+            @error('image_path')
+                <p class="sell__error">{{ $message }}</p>
+            @enderror
         </div>
+
         <!-- 商品詳細 -->
-        <div class="sell__detail">
-            <div class="sell__detail-title">
-                <h3>商品の詳細</h3>
-            </div>
-            <div class="sell__detail-category">
+        <div class="sell__section">
+            <h3 class="sell__section-title">商品の詳細</h3>
+
+            <div class="sell__group">
+                <label class="sell__group-label">カテゴリー</label>
                 @foreach($categories as $category)
-                    <label class="category-select__label">
+                    <label class="sell__category-item">
                         <input type="checkbox" name="category_ids[]" value="{{ $category->id }}" {{ in_array($category->id, old('category_ids', [])) ? 'checked' : '' }}>
-                        <span class="sell__category-button">{{ $category->name }}</span>
+                        <span class="sell__detail-category-button">{{ $category->name }}</span>
                     </label>
                 @endforeach
             </div>
+            
+            <div class="sell__detail-condition">
+                <label class="sell__detail-category-label">商品の状態</label>
+                <select name="condition" id="">
+                    <option value="1">良好</option>
+                    <option value="2">目立った傷や汚れなし</option>
+                    <option value="3">やや傷や汚れあり</option>
+                    <option value="4">状態が悪い</option>
+                </select>
+
+            </div>
+                
+
+                </div>
 
         </div>
     </form>
