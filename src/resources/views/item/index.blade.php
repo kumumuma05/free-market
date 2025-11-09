f72c94c86d50631c4d509412fe4dfc18f631f894
kumub <ma.okamoto.yf7@gmail.com>
2025-11-09 22:09:52 +0900

    商品一覧画面チェック

--- src/resources/views/item/index.blade.php
+++ src/resources/views/item/index.blade.php
@@ -9,32 +9,33 @@
 
         <!-- セッションメッセージ表示 -->
         @if(session('status'))
-            <div class="item-index__alert" >
+            <div class="item-index__alert">
                 {{ session('status') }}
             </div>
         @endif
 
         <!-- タブ -->
-        <div class="tab-menu">
-            <a class="tab-menu__link {{ $activeTab === 'recommend' ? 'tab-menu__link--active' : '' }}" href="/">おすすめ</a>
-            <a class="tab-menu__link {{ $activeTab === 'mylist' ? 'tab-menu__link--active' : '' }}" href="/?tab=mylist&keyword={{ urlencode($keyword ?? request('keyword', '' )) }}">マイリスト</a>
+        <div class="item-index__tab-menu">
+            <a class="item-index__tab-menu-link {{ $activeTab === 'recommend' ? 'tab-menu__link--active' : '' }}" href="/">おすすめ</a>
+            <a class="item-index__tab-menu-link {{ $activeTab === 'mylist' ? 'tab-menu__link--active' : '' }}" href="/?tab=mylist&keyword={{ urlencode($keyword ?? request('keyword', '' )) }}">マイリスト</a>
         </div>
 
         <!-- 一覧 -->
-        <ul class="item-list">
+        <ul class="item-index__list">
             @foreach($items as $item)
-                <li class="item-list__item">
-                    <a class="item-list__card" href="/item/{{ $item->id }}">
-                        <div class="item-list__card-image">
+                <li class="item-index__list-item">
+                    <a class="item-index__list-card" href="/item/{{ $item->id }}">
+                        <div class="item-index__list-card-image">
                             <img src="{{ $item->image_url }}" alt="商品画像">
                             @if($item->is_sold)
-                                <span class="item-list__card-sold">Sold</span>
+                                <span class="item-index__list-card-sold">Sold</span>
                             @endif
                         </div>
-                        <p class="item-list__card-title">{{ $item->product_name }}</p>
+                        <p class="item-index__list-card-title">{{ $item->product_name }}</p>
                     </a>
                 </li>
             @endforeach
         </ul>
+
     </div>
 @endsection
