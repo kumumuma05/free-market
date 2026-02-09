541a820ad30fbd18c8e8c8e88316f172cc3b5c35
kumub <ma.okamoto.yf7@gmail.com>
2026-02-09 22:12:41 +0900

    create new table

---
+++ src/database/migrations/2026_02_09_214026_create_ratings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('purchase_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('rater_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('ratee_id')->constrained('users')->cascadeOnDelete();
+            $table->unsignedTinyInteger('score');
+            $table->timestamps();
+
+            $table->unique(['purchase_id', 'rater_id']);
+            $table->index(['ratee_id', 'score']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
